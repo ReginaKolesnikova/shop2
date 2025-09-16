455be6103212aa19d9b37a54b1ea55c55888fcbc
ReginaKolesnikova <151017512+ReginaKolesnikova@users.noreply.github.com>
2025-09-16 18:28:20 +0300

    Add files via upload

---
+++ Controllers/SpaceshipsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopTARgv24.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        public readonly ShopTARgv24Context _context;
+
+        public SpaceshipsController 
+            (
+            ShopTARgv24Context context
+            )
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.Spaceships
+                .Select(x => new SpaceshipsIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    TypeName = x.TypeName,
+                    BuiltDate = x.BuiltDate,
+                    Crew = x.Crew
+                });
+
+            return View(result);
+        }
+    }
+}
